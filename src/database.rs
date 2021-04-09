379c71767da616320f502fe52954dbbcdb5dd9c7
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-04-09 03:13:37 -0600

    Fix OpenSSL DB integration

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,6 @@
-use postgres::{Client, NoTls, Row, Error, Config};
+use postgres::{Client, Row, Error};
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
+use postgres_openssl::MakeTlsConnector;
 
 pub struct Database {
 	client: Client
@@ -6,7 +8,18 @@ pub struct Database {
 
 impl Database {
 	pub fn new() -> Result<Database, String> {
+<<<<<<< HEAD
 		let client = Client::connect("host=ec2-52-7-115-250.compute-1.amazonaws.com dbname=deb6b3ucs3l9cg user=pltegjiermmuku port=5432 password=placeholder", NoTls).map_err(|_| "Connection error")?;
+=======
+		let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+		builder.set_verify(SslVerifyMode::NONE);
+		let connector = MakeTlsConnector::new(builder.build());
+
+		let client = Client::connect(
+			"host=ec2-52-7-115-250.compute-1.amazonaws.com dbname=deb6b3ucs3l9cg user=pltegjiermmuku port=5432 password=placeholder", 
+			connector
+		).map_err(|_| "Connection error")?;
+>>>>>>> fbe9392... Fix OpenSSL DB integration
 
 		Ok(Database { client })
 	}
