472dbecaa1f2791fe59285875827cbacd6905bcf
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-04-09 15:57:55 -0600

    Put password in separate file

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use postgres::{Client, Row, Error};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use postgres_openssl::MakeTlsConnector;
@@ -8,18 +10,14 @@ pub struct Database {
 
 impl Database {
 	pub fn new() -> Result<Database, String> {
-<<<<<<< HEAD
-		let client = Client::connect("host=ec2-52-7-115-250.compute-1.amazonaws.com dbname=deb6b3ucs3l9cg user=pltegjiermmuku port=5432 password=placeholder", NoTls).map_err(|_| "Connection error")?;
-=======
 		let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
 		builder.set_verify(SslVerifyMode::NONE);
 		let connector = MakeTlsConnector::new(builder.build());
 
 		let client = Client::connect(
-			"host=ec2-52-7-115-250.compute-1.amazonaws.com dbname=deb6b3ucs3l9cg user=pltegjiermmuku port=5432 password=placeholder", 
+			format!("host=ec2-52-7-115-250.compute-1.amazonaws.com dbname=deb6b3ucs3l9cg user=pltegjiermmuku port=5432 password={}", fs::read_to_string("db.info").unwrap()), 
 			connector
 		).map_err(|_| "Connection error")?;
->>>>>>> fbe9392... Fix OpenSSL DB integration
 
 		Ok(Database { client })
 	}
