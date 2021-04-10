8c6a533e8f6eab9153434ffcde0b02de34985d5e
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-04-09 18:14:12 -0600

    Fix JSON response

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ fn handle_connection(db: Arc<Mutex<Database>>, mut stream: TcpStream) {
 				println!("Person: {} {} {}", id, username, email);
 
 				let response = format!(
-					"{}\r\n\r\n{}\r\n{{ {{ \"id\" : \"{}\", \"username\" : \"{}\", \"email\" : \"{}\" }} }}",
+					"{}\r\n{}\r\n{{ {{ \"id\" : \"{}\", \"username\" : \"{}\", \"email\" : \"{}\" }} }}",
 					"HTTP/1.1 200 OK",
 					"Content-Type: application/json;",
 					id,
