1b39942e51d3ee51d55eef63272d350b4227403f
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-04-09 18:47:16 -0600

    Fix formatting again

--- src/main.rs
+++ src/main.rs
@@ -67,14 +67,7 @@ fn handle_connection(db: Arc<Mutex<Database>>, mut stream: TcpStream) {
 				println!("Person: {} {} {}", id, username, email);
 
 				let response = format!(
-					"{}
-					{}
-					
-					{{
-						\"id\" : \"{}\",
-						\"username\" : \"{}\",
-						\"email\" : \"{}\"
-					}}",
+					"{}\r\n{}\r\n\r\n{{\r\n\"id\" : \"{}\",\r\n\"username\" : \"{}\",\r\n\"email\" : \"{}\"\r\n}}",
 					"HTTP/1.1 200 OK",
 					"Content-Type: application/json; charset=UTF-8",
 					id,
