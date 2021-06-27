977de5dbbcacbe4ed12002fb7659f6d053042e01
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-06-27 00:08:18 -0600

    Real build

---
+++ srv/build/About-ed9e148f.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,v as a,s as e,e as n,a as o,b as r,B as c,h as u}from"./vendor-4f00c5b8.js";function i(s){let t,a;return{c(){t=e(),a=n("main"),a.innerHTML="<p>About</p>",document.title="About",o(a,"class","svelte-gv2hc7")},m(s,e){r(s,t,e),r(s,a,e)},p:c,i:c,o:c,d(s){s&&u(t),s&&u(a)}}}export default class extends s{constructor(s){super(),t(this,s,null,i,a,{})}}
+//# sourceMappingURL=About-ed9e148f.js.map
--- srv/build/main.js
+++ srv/build/main.js
@@ -1,5 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { a as default } from './main2.js';
-import './vendor.js';
+export{a as default}from"./main-1841df6d.js";import"./vendor-4f00c5b8.js";
 //# sourceMappingURL=main.js.map
