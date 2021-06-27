39312c7232c096019419d4815b1ee23803d509ad
Drew Christensen <emoryandrewchristensen@gmail.com>
2021-06-27 16:42:26 -0600

    Add to gitignore as part of introducing build script

--- srv/build/Contact-6dde2029.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,v as a,s as e,e as n,a as c,b as o,B as r,h as i}from"./vendor-4f00c5b8.js";function l(s){let t,a;return{c(){t=e(),a=n("main"),a.innerHTML="<p>Contact</p>",document.title="Contact",c(a,"class","svelte-gv2hc7")},m(s,e){o(s,t,e),o(s,a,e)},p:r,i:r,o:r,d(s){s&&i(t),s&&i(a)}}}export default class extends s{constructor(s){super(),t(this,s,null,l,a,{})}}
-//# sourceMappingURL=Contact-6dde2029.js.map
--- srv/build/main.js
+++
@@ -1,2 +0,0 @@
-export{a as default}from"./main-1841df6d.js";import"./vendor-4f00c5b8.js";
-//# sourceMappingURL=main.js.map
